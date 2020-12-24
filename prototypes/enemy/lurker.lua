9bb68edade8bd65c74b2beaa6e5377dc0653f041
heyqule <hq.jfax@gmail.com>
2020-12-24 00:40:53 -0500

    Move files to root All buildings and units are now functional

---
+++ prototypes/enemy/lurker.lua
@@ -0,0 +1,223 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: heyqule
+-- Date: 12/19/2020
+-- Time: 7:14 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require('__stdlib__/stdlib/utils/defines/time')
+
+local ERM_UnitHelper = require('__enemyracemanager__/lib/unit_helper')
+local ERM_UnitTint = require('__enemyracemanager__/lib/unit_tint')
+local ZergSound = require('__erm_zerg__/prototypes/sound')
+
+local name = 'lurker'
+
+local health_multiplier = settings.startup["enemyracemanager-level-multipliers"].value
+local hitpoint = 125
+local max_hitpoint_multiplier = settings.startup["enemyracemanager-max-hitpoint-multipliers"].value * 1.5
+
+local resistance_mutiplier = settings.startup["enemyracemanager-level-multipliers"].value
+-- Handles acid and poison resistance
+local base_acid_resistance = 25
+local incremental_acid_resistance = 65
+-- Handles physical resistance
+local base_physical_resistance = 0
+local incremental_physical_resistance = 90
+-- Handles fire and explosive resistance
+local base_fire_resistance = 10
+local incremental_fire_resistance = 80
+-- Handles laser and electric resistance
+local base_eletric_resistance = 0
+local incremental_eletric_resistance = 90
+-- Handles cold resistance
+local base_cold_resistance = 25
+local incremental_cold_resistance = 65
+
+-- Handles acid damages
+local damage_multiplier = settings.startup["enemyracemanager-level-multipliers"].value
+local base_physical_damage = 20
+local incremental_physical_damage = 55
+
+-- Handles Attack Speed
+local attack_speed_multiplier = settings.startup["enemyracemanager-level-multipliers"].value
+local base_attack_speed = 200
+local incremental_attack_speed = 90
+
+local attack_range = 16
+
+local movement_multiplier = settings.startup["enemyracemanager-level-multipliers"].value
+local base_movement_speed = 0.15
+local incremental_movement_speed = 0.075
+
+-- Misc settings
+local vision_distance = 48
+local pollution_to_join_attack = 50
+local distraction_cooldown = 20
+
+-- Animation Settings
+local unit_scale = 1.3
+
+local selection_box = { { -1.0, -1.0 }, { 1.0, 1.0 } }
+
+function ErmZerg.make_lurker(level)
+    level = level or 1
+
+    data:extend({
+        {
+            type = "unit",
+            name = MOD_NAME..'-'..name .. '-' .. level,
+            icon = "__erm_zerg__/graphics/entity/icons/units/" .. name .. ".png",
+            icon_size = 64,
+            flags = { "placeable-enemy", "placeable-player", "placeable-off-grid", "breaths-air"},
+            has_belt_immunity = false,
+            max_health = ERM_UnitHelper.get_health(hitpoint, hitpoint * max_hitpoint_multiplier, health_multiplier, level),
+            order = "erm-" .. name .. '-' .. level,
+            subgroup = "enemies",
+            shooting_cursor_size = 2,
+            resistances = {
+                { type = "acid", percent = ERM_UnitHelper.get_resistance(base_acid_resistance, incremental_acid_resistance, resistance_mutiplier, level)},
+                { type = "poison", percent = ERM_UnitHelper.get_resistance(base_acid_resistance, incremental_acid_resistance, resistance_mutiplier, level) },
+                { type = "physical", percent = ERM_UnitHelper.get_resistance(base_physical_resistance, incremental_physical_resistance, resistance_mutiplier, level)},
+                { type = "fire", percent = ERM_UnitHelper.get_resistance(base_fire_resistance, incremental_fire_resistance, resistance_mutiplier, level)},
+                { type = "explosion", percent = ERM_UnitHelper.get_resistance(base_fire_resistance, incremental_fire_resistance, resistance_mutiplier, level)},
+                { type = "laser", percent = ERM_UnitHelper.get_resistance(base_eletric_resistance, incremental_eletric_resistance, resistance_mutiplier, level)},
+                { type = "electric", percent = ERM_UnitHelper.get_resistance(base_eletric_resistance, incremental_eletric_resistance, resistance_mutiplier, level)},
+                { type = "cold", percent = ERM_UnitHelper.get_resistance(base_cold_resistance, incremental_cold_resistance, resistance_mutiplier, level)}
+            },
+            healing_per_tick = ERM_UnitHelper.get_healing(hitpoint, max_hitpoint_multiplier, health_multiplier, level),
+            --collision_mask = { "player-layer" },
+            collision_box = selection_box,
+            selection_box = selection_box,
+            sticker_box = selection_box,
+            vision_distance = vision_distance,
+            movement_speed = ERM_UnitHelper.get_movement_speed(base_movement_speed, incremental_movement_speed, movement_multiplier, level),
+            pollution_to_join_attack = pollution_to_join_attack,
+            distraction_cooldown = distraction_cooldown,
+            ai_settings = biter_ai_settings,
+            attack_parameters = {
+                type = "projectile",
+                ammo_category = 'biological',
+                range = attack_range,
+                cooldown = ERM_UnitHelper.get_attack_speed(base_attack_speed, incremental_attack_speed, attack_speed_multiplier, level),
+                cooldown_deviation = 0.1,
+                warmup = 30,
+                ammo_type = {
+                    category = "biological",
+                    target_type = "direction",
+                    action = {
+                        type = "area",
+                        radius = 3,
+                        force = "enemy",
+                        ignore_collision_condition = true,
+                        action_delivery = {
+                            type = "instant",
+                            target_effects =
+                            {
+                                {
+                                    type="create-explosion",
+                                    entity_name = 'lurker-explosion'
+                                },
+                                {
+                                    type = "damage",
+                                    damage = {amount = ERM_UnitHelper.get_damage(base_physical_damage, incremental_physical_damage, damage_multiplier, level), type = "physical"}
+                                },
+                            }
+                        }
+                    },
+                },
+                sound = ZergSound.lurker_attack(0.75),
+                animation = {
+                    layers = {
+                        {
+                            filename = "__erm_zerg__/graphics/entity/units/" .. name .. "/" .. name .. "-burrow-combined.png",
+                            width = 128,
+                            height = 128,
+                            line_length = 9,
+                            frame_count = 18,
+                            axially_symmetrical = false,
+                            direction_count = 1,
+                            scale = unit_scale,
+                            animation_speed = 0.5
+                        },
+                        {
+                            filename = "__erm_zerg__/graphics/entity/units/" .. name .. "/" .. name .. "-burrow-combined-mask.png",
+                            width = 128,
+                            height = 128,
+                            line_length = 9,
+                            frame_count = 18,
+                            axially_symmetrical = false,
+                            direction_count = 1,
+                            flags = { "mask" },
+                            scale = unit_scale,
+                            tint = ERM_UnitTint.tint_shadow(),
+                            draw_as_shadow = true,
+                            animation_speed = 0.5
+                        }
+                    }
+                }
+            },
+
+            distance_per_frame = 0.17,
+            run_animation = {
+                layers = {
+                    {
+                        filename = "__erm_zerg__/graphics/entity/units/" .. name .. "/" .. name .. "-run.png",
+                        width = 128,
+                        height = 128,
+                        frame_count = 6,
+                        axially_symmetrical = false,
+                        direction_count = 16,
+                        scale = unit_scale,
+                        animation_speed = 0.6,
+                    },
+                    {
+                        filename = "__erm_zerg__/graphics/entity/units/" .. name .. "/" .. name .. "-run-mask.png",
+                        width = 128,
+                        height = 128,
+                        frame_count = 6,
+                        axially_symmetrical = false,
+                        direction_count = 16,
+                        flags = { "mask" },
+                        scale = unit_scale,
+                        tint = ERM_UnitTint.tint_shadow(),
+                        draw_as_shadow = true,
+                        animation_speed = 0.6,
+                    }
+                }
+            },
+            dying_explosion = "blood-explosion-small",
+            dying_sound = {
+                filename = "__erm_zerg__/sound/enemies/" .. name .. "/death.ogg",
+                volume = 1
+            },
+            corpse = name .. '-corpse'
+        },
+        {
+            type = "corpse",
+            name = name .. '-corpse',
+            icon = "__erm_zerg__/graphics/entity/icons/units/" .. name .. ".png",
+            icon_size = 64,
+            flags = { "placeable-off-grid", "building-direction-8-way", "not-on-map" },
+            selection_box = selection_box,
+            selectable_in_game = false,
+            dying_speed = 0.04,
+            time_before_removed = defines.time.minute * settings.startup["enemyracemanager-enemy-corpse-time"].value,
+            subgroup = "corpses",
+            order = "x" .. name .. level,
+            final_render_layer = "lower-object-above-shadow",
+            animation = {
+                filename = "__erm_zerg__/graphics/entity/units/" .. name .. "/" .. name .. "-death.png",
+                width = 128,
+                height = 128,
+                frame_count = 6,
+                direction_count = 1,
+                axially_symmetrical = false,
+                scale = unit_scale,
+                animation_speed = 0.2
+            },
+        },
+    })
+
+end
---
+++ prototypes/sound.lua
@@ -0,0 +1,267 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: heyqule
+-- Date: 12/17/2020
+-- Time: 1:04 AM
+-- To change this template use File | Settings | File Templates.
+--
+local ZergSound = {}
+
+function ZergSound.meele_attack(volume)
+    return
+    {
+        audible_distance_modifier = 0.5,
+        variations =
+        {
+            {
+                filename = "__erm_zerg__/sound/enemies/zergling/attack.ogg",
+                volume = volume
+            },
+            {
+                filename = "__erm_zerg__/sound/enemies/ultralisk/attack.ogg",
+                volume = volume
+            }
+        }
+    }
+end
+
+function ZergSound.mutalisk_attack(volume)
+    return
+    {
+        audible_distance_modifier = 0.5,
+        variations =
+        {
+            {
+                filename = "__erm_zerg__/sound/enemies/mutalisk/attack.ogg",
+                volume = volume
+            }
+        }
+    }
+end
+
+function ZergSound.hydralisk_attack(volume)
+return
+{
+    audible_distance_modifier = 0.5,
+    variations =
+    {
+        {
+            filename = "__erm_zerg__/sound/enemies/hydralisk/attack.ogg",
+            volume = volume
+        }
+    }
+}
+end
+
+function ZergSound.devourer_attack(volume)
+    return
+    {
+        audible_distance_modifier = 0.5,
+        variations =
+        {
+            {
+                filename = "__erm_zerg__/sound/enemies/devourer/attack.ogg",
+                volume = volume
+            }
+        }
+    }
+end
+
+function ZergSound.building_working_sound(name, volume)
+    return
+    {
+        audible_distance_modifier = 1,
+        max_sounds_per_type = 3,
+        sound =
+        {
+            filename = "__erm_zerg__/sound/buildings/"..name..".ogg",
+            volume = volume
+        },
+        probability = 1 / (15 * 60)
+    }
+end
+
+function ZergSound.building_dying_sound(volume)
+    return
+    {
+        audible_distance_modifier = 1,
+        filename = "__erm_zerg__/sound/buildings/building_death.ogg",
+        volume = volume
+    }
+end
+
+function ZergSound.devourer_hit(volume)
+    return
+    {
+        {
+            filename = "__erm_zerg__/sound/enemies/devourer/goohit.ogg",
+            volume = volume
+        }
+    }
+end
+
+function ZergSound.overlord_drop(volume)
+    return
+    {
+        {
+            filename = "__erm_zerg__/sound/enemies/overlord/attack.ogg",
+            volume = volume
+        }
+    }
+end
+
+function ZergSound.lurker_hit(volume)
+    return
+    {
+        {
+            filename = "__erm_zerg__/sound/enemies/lurker/hit-1.ogg",
+            volume = volume
+        },
+        {
+            filename = "__erm_zerg__/sound/enemies/lurker/hit-2.ogg",
+            volume = volume
+        },
+        {
+            filename = "__erm_zerg__/sound/enemies/lurker/hit-3.ogg",
+            volume = volume
+        }
+
+    }
+end
+
+function ZergSound.lurker_attack(volume)
+    return
+    {
+        audible_distance_modifier = 0.5,
+        variations =
+        {
+            {
+                filename = "__erm_zerg__/sound/enemies/lurker/attack-1.ogg",
+                volume = volume
+            },
+            {
+                filename = "__erm_zerg__/sound/enemies/lurker/attack-2.ogg",
+                volume = volume
+            }
+        }
+    }
+end
+
+function ZergSound.sunker_idle(volume)
+    return {
+        audible_distance_modifier = 0.5,
+        sound = {
+            {
+                filename = "__erm_zerg__/sound/buildings/sunker_colony.ogg",
+                volume = volume
+            },
+        },
+        probability = 1 / (15 * 60),
+        max_sounds_per_type = 3,
+    }
+end
+
+function ZergSound.sunker_attack(volume)
+    return {
+        audible_distance_modifier = 1,
+        variations = {
+            {
+                filename = "__erm_zerg__/sound/buildings/sunker_attack.ogg",
+                volume = volume
+            },
+        }
+    }
+end
+
+function ZergSound.sunker_hit(volume)
+    return {
+        audible_distance_modifier = 0.5,
+        variations = {
+            {
+                filename = "__erm_zerg__/sound/buildings/sunker_hit.ogg",
+                volume = volume
+            },
+        }
+    }
+end
+
+function ZergSound.spore_idle(volume)
+    return {
+        audible_distance_modifier = 0.5,
+        sound = {
+            {
+                filename = "__erm_zerg__/sound/buildings/spore_colony.ogg",
+                volume = volume
+            },
+        },
+        max_sounds_per_type = 3,
+        probability = 1 / (15 * 60),
+    }
+end
+
+function ZergSound.infested_attack(volume)
+    return {
+        audible_distance_modifier = 0.5,
+        variations = {
+            {
+                filename = "__erm_zerg__/sound/enemies/infested/attack.ogg",
+                volume = volume
+            },
+            {
+                filename = "__erm_zerg__/sound/enemies/infested/attack-2.ogg",
+                volume = volume
+            },
+            {
+                filename = "__erm_zerg__/sound/enemies/infested/attack-3.ogg",
+                volume = volume
+            },
+        }
+    }
+end
+
+
+function ZergSound.infested_death(volume)
+    return {
+        audible_distance_modifier = 0.5,
+        variations = {
+            {
+                filename = "__erm_zerg__/sound/enemies/infested/death.ogg",
+                volume = volume
+            },
+            {
+                filename = "__erm_zerg__/sound/enemies/infested/death-1.ogg",
+                volume = volume
+            }
+        }
+    }
+end
+
+function ZergSound.defiler_attack(volume)
+    return
+    {
+        audible_distance_modifier = 0.5,
+        variations =
+        {
+            {
+                filename = "__erm_zerg__/sound/enemies/defiler/attack.ogg",
+                volume = volume
+            }
+        }
+    }
+end
+
+function ZergSound.defiler_deat(volume)
+    return
+    {
+        audible_distance_modifier = 0.5,
+        variations =
+        {
+            {
+                filename = "__erm_zerg__/sound/enemies/hydralisk/attack.ogg",
+                volume = volume
+            }
+        }
+    }
+end
+
+return ZergSound
