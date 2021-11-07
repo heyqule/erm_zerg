eec5d24e5725390506734fda24bf2198acd5af24
heyqule <hq.jfax@gmail.com>
2021-11-07 12:43:33 -0500

    Fix subgroup add erm-builder-enemies subgroup

--- prototypes/enemy/overlord.lua
+++ prototypes/enemy/overlord.lua
@@ -77,7 +77,7 @@ function ErmZerg.make_overlord(level)
             has_belt_immunity = true,
             max_health = ERM_UnitHelper.get_health(hitpoint, hitpoint * max_hitpoint_multiplier, health_multiplier, level),
             order = MOD_NAME .. '/'  .. name .. '/' .. level,
-            subgroup = "dropship-enemies",
+            subgroup = "erm-dropship-enemies",
             shooting_cursor_size = 2,
             resistances = {
                 { type = "acid", percent = ERM_UnitHelper.get_resistance(base_acid_resistance, incremental_acid_resistance, resistance_mutiplier, level) },
