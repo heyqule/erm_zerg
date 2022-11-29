20d7bfa7b3130bb5ab3e2bb4672cbb93aaf0ae39
heyqule <hq.jfax@gmail.com>
2022-11-28 20:53:10 -0500

    - process_attack can process without global.boss. - despawn can attack in multiple spread

--- scripts/boss_attacks.lua
+++ scripts/boss_attacks.lua
@@ -73,8 +73,8 @@ ErmBossAttackRemote.despawn_attacks =
         ErmBossAttackProcessor.TYPE_PROJECTILE,
     },
     projectile_chance = {100},
-    projectile_count = {5},
-    projectile_spread = {1},
+    projectile_count = {3},
+    projectile_spread = {3},
     projectile_use_multiplier = {false},
     projectile_count_multiplier = {
         {},
