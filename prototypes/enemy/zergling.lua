58ce8374b795f9496a97c260c32f98ab765879e6
heyqule <hq.jfax@gmail.com>
2023-01-01 21:52:14 -0500

    Added pollution to join attack multiplier to mod's start up setting

--- prototypes/enemy/zergling.lua
+++ prototypes/enemy/zergling.lua
@@ -96,7 +96,7 @@ function ErmZerg.make_zergling(level)
             sticker_box = selection_box,
             vision_distance = vision_distance,
             movement_speed = ERM_UnitHelper.get_movement_speed(base_movement_speed, incremental_movement_speed,  level),
-            pollution_to_join_attack = pollution_to_join_attack,
+            pollution_to_join_attack = ERM_UnitHelper.get_pollution_attack(pollution_to_join_attack, level),
             distraction_cooldown = distraction_cooldown,
             ai_settings = biter_ai_settings,
             attack_parameters = {
