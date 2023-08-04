97463d8c688c9e452f601e573915920e8fadfee6
heyqule <hq.jfax@gmail.com>
2023-08-04 10:27:24 -0400

    Autoplace refactor

--- data.lua
+++ data.lua
@@ -5,8 +5,8 @@ require('__erm_zerg__/global')
 local ErmConfig = require('__enemyracemanager__/lib/global_config')
 
 -- This set of data is used for set up default autoplace calculation.
-data.erm_enemy_races = data.erm_enemy_races or {}
-table.insert(data.erm_enemy_races, {
+data.erm_spawn_specs = data.erm_spawn_specs or {}
+table.insert(data.erm_spawn_specs, {
     name=MOD_NAME,
     force=FORCE_NAME,
     moisture=1, -- 1 = Dry and 2 = Wet
