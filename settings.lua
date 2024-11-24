d4642a8b4b94022c58da09ad3f7cfa2914c16fc7
heyqule <hq.jfax@gmail.com>
2024-11-23 23:21:01 -0500

    - Turn off new forces on vulcanus and fulgora by default - locale changes

--- settings.lua
+++ settings.lua
@@ -65,7 +65,7 @@ if feature_flags.space_travel then
             name = "enemy_erm_zerg-on_vulcanus",
             description = "enemy_erm_zerg-on_vulcanus",
             setting_type = "startup",
-            default_value = true,
+            default_value = false,
             order = "enemy_erm_zerg-200",
         },
     }
